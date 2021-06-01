95047b64de4223d5d9cb1e76d0756872aa444892
shallovv <shallovv@users.noreply.github.com>
2021-06-01 16:30:09 +0000

    deploy: 3ea6be3c7da440dfafe1c16847f0f194911251fc

---
+++ _nuxt/static/1622565008/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsample-portfolio\u002F_nuxt\u002Fstatic\u002F1622565008",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fsample-portfolio\u002F",assetsPath:"\u002Fsample-portfolio\u002F_nuxt\u002F",cdnURL:a}}}}(null));
