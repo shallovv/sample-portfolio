3ea6be3c7da440dfafe1c16847f0f194911251fc
shallovv <miomio.ll.8507@gmail.com>
2021-06-02 01:23:34 +0900

    Add tailwindcss

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,24 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        sample-portfolio
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <!DOCTYPE html>
+  <html lang="ja">
+    <head>
+      <title>sample-portfolio</title>
+    </head>
+    <body>
+      <main>
+        <section>
+          <h1 class="text-center text-2xl">shallovv</h1>
+          <h2 class="text-center text-lg">Link</h2>
+          <ul class="text-center text-base">
+            <li>
+              <a href="https://github.com/shallovv">GitHub</a>
+            </li>
+            <li>
+              <a href="https://shallovv.hateblo.jp">Blog</a>
+            </li>
+          </ul>
+        </section>
+      </main>
+    </body>
+  </html>
 </template>
-
-<script>
-export default {}
-</script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
